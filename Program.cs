8d9e623c7efd1409b9a83a1abaae936329924331
Antro <71667408+JackWilldoo@users.noreply.github.com>
2021-09-24 02:23:09 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
